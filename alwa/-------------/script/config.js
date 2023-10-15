a8c733178b3592c7650bca6b848bd097c026932e
Alwafauzan <alwafauzan123@gmail.com>
2023-10-15 07:38:55 +0700

    a

--- alwa/-------------/script/config.js
+++ alwa/-------------/script/config.js
@@ -1,9 +1,10 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyCvHWmmyIRF5uvzHSOp92UnE-0-yPCmgBo",
-    authDomain: "emgrealtime.firebaseapp.com",
-    databaseURL: "https://emgrealtime-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "emgrealtime",
-    storageBucket: "emgrealtime.appspot.com",
-    messagingSenderId: "806224184787",
-    appId: "1:806224184787:web:65b970d3c29521e314db26"
+  apiKey: "AIzaSyB4Ee_IPBbVhkavoXXrJ_bg7YKK5rfVc2Y",
+  authDomain: "tes-plot.firebaseapp.com",
+  databaseURL: "https://tes-plot-default-rtdb.firebaseio.com",
+  projectId: "tes-plot",
+  storageBucket: "tes-plot.appspot.com",
+  messagingSenderId: "562009303458",
+  appId: "1:562009303458:web:6ad900e6c71bec229e2afb",
+  measurementId: "G-ZV7GZPRH8R"
   };
--- alwa/-------------/script/script.js
+++ alwa/-------------/script/script.js
@@ -1,58 +1,63 @@
+const ppg_div = document.querySelector(`[data-firebase="ppg"]`)
+
+
 firebase.initializeApp(firebaseConfig);
 var dbRef = firebase.database();
-var dataaa = dbRef.ref("emg");
+var dataaa = dbRef.ref("data");
 dataaa.on("value", (snap) => {
-    // console.log(JSON.stringify(snap))
+    const ppg = snap.child('ir');
+    const millis = snap.child('millis');
+    // console.log(JSON.stringify(snap.child('ir')));
     // console.log(snap.numChildren())
-    let emg_val = [];
-    // var timestamp_val
-    // var servo_val
-    snap.forEach((emg) => {
-        //   emg_val += JSON.stringify(emg.val().emg)
-        //   timestamp_val += JSON.stringify(emg.val().timestamp)
-        //   servo_val += emg.val()
-        //   console.log(JSON.stringify(emg.servo))
-        // emg_val += emg.val().emg;
-        // console.log(emg.val().emg)
-        emg_val += emg.val().emg
-    });
-    console.log(emg_val)
-    // console.log(timestamp_val)
-    // console.log(servo_val)
-    // console.log([1,2,3])
+    // snap.forEach((data) => {
+    //     //   emg_val += JSON.stringify(emg.val().emg)
+    //     //   servo_val += emg.val()
+    //     //   console.log(JSON.stringify(ppg))
+    //     // ppg_val += data.val();
+    //     // console.log(data.val())
+    //     // emg_val += emg.val().emg
+    //     ppg_div.innerHTML += (`
+    //     <div>${snap.val()}</div>
+    //     `)
+    // });
     const ctx = document.getElementById('myChart').getContext('2d');
             
             var myChart = new Chart(ctx, {
             type: 'line',
             data: {
-                labels: emg_val,
+                labels: millis.val(),
                 datasets: [{
-                    label: '# of Votes',
-                    data: emg_val,
+                    label: 'PPG',
+                    data: ppg.val(),
                     backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    // 'rgba(255, 99, 132, 0.2)',
+                    // 'rgba(54, 162, 235, 0.2)',
+                    // 'rgba(255, 206, 86, 0.2)',
+                    // 'rgba(75, 192, 192, 0.2)',
+                    // 'rgba(153, 102, 255, 0.2)',
+                    // 'rgba(255, 159, 64, 0.2)'
                     ],
                     borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    // 'rgba(255,99,132,1)',
+                    // 'rgba(54, 162, 235, 1)',
+                    // 'rgba(255, 206, 86, 1)',
+                    // 'rgba(75, 192, 192, 1)',
+                    // 'rgba(153, 102, 255, 1)',
+                    // 'rgba(255, 159, 64, 1)'
                     ],
-                    borderWidth: 1
+                    // borderWidth: 1
                 }]
             },
             options: {
                 scales: {
+                    xAxes: [{
+                        ticks: {
+                            beginAtZero:false
+                        }
+                    }],
                     yAxes: [{
                         ticks: {
-                            beginAtZero:true
+                            beginAtZero:false
                         }
                     }]
                 }
